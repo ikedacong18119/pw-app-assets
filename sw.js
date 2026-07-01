19090ae101dbd0edd2173102bdd08123ad2a0d8c
ikedacong18119 <kofuikedacong@gmail.com>
2026-07-01 10:19:33 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pw-app-shell-v2';
+const CACHE_NAME = 'pw-app-shell-v3';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './pw_icon.png'];
 
 self.addEventListener('install', event => {
